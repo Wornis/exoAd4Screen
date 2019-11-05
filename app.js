e03bfa671a78ab135030f31f16225c835f30b3f3
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-11-05 15:53:49 +0100

    hello

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let apiRouter = require('./apiRouter').router
 
 let app = express()
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use((req, res, next) => console.log(req.body) || next());
 app.use(apiRouter);
 app.listen(process.env.PORT || 3002) // Ecoute sur le port de developpement sinon 3002 en local
