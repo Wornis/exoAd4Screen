7d9f7828d6239a7ac540c6448ef340ab5781d860
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-11-05 17:00:19 +0100

    easya

--- apiRouter.js
+++ apiRouter.js
@@ -2,7 +2,7 @@ let express = require('express')
 let userCtrl = require('./routes/user/userCtrl');
 
 module.exports.router = (() => {
-    const apiRouter = express.Router()
+    const apiRouter = express.Router();
 
     apiRouter.route('/user/:id').get(userCtrl.getUserViewedArticles);
     apiRouter.route('/sync_project').post(userCtrl.webhook);
