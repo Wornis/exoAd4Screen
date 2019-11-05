04326dad52472183549a93f9e23a26b0338382f3
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-11-05 16:52:08 +0100

    easy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-let apiRouter = require('./apiRouter').router
+let apiRouter = require('./apiRouter').router;
 
 let app = express();
 app.use(bodyParser.urlencoded({extended: false}))
